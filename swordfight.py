ebf2eb918159cb26a478d24e25e0d593b1f9b32e
linus <flyrhinosfly@gmail.com>
2025-01-20 10:02:50 -0500

    change get entity-manager submodule to work

--- swordfight.py
+++ swordfight.py
@@ -1,6 +1,9 @@
 ## @package swordfight
 
 import pyray as pr
+import entity_manager.entity_manager as em
+
+myManager = em.Entity_manager
 
 pr.set_target_fps(60)
 pr.init_window(800,450, 'swordfight')
